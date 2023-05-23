8df4f56ff8162f0ca16d28b5ab3426a2fcebd9e3
hehehiyahayu <arturozayn@gmail.com>
2023-05-23 11:16:28 +0700

    Dokumentasi komentar

--- Cat_Detector.py
+++ Cat_Detector.py
@@ -2,8 +2,9 @@ import cv2
 import streamlit as st
 import numpy as np
 
-# Importing HARR CASCADE XML file
+# Digunakan untuk melakukan impor data haarcascade dari file xml untuk data muka kucing.
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+# Digunakan untuk melakukan impor data haarcascade dari file xml untuk data muka manusia.
 human_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # CSS styling
@@ -23,56 +24,66 @@ text2 = "Anda dapat menggunakan aplikasi ini untuk mengamati keberadaan wajah ma
 
 
 def main():
-
+    # Digunakan untuk memberikan title pada halaman.
     st.title("Real-Time Human and Cat Face Detector")
+    # Digunakan untuk menampilkan data penjelesan dari variable text1 diatas.
     st.write(f"<p class='text-justify'>{text1}</p>", unsafe_allow_html=True)
+    # Digunakan untuk menampilkan data penjelesan dari variable text2 diatas.
     st.write(f"<p class='text-justify'>{text2}</p>", unsafe_allow_html=True)
     st.write("Tekan tombol Mulai untuk memulai :) ")
+
+    # Digunakan untuk mendeteksi event berupa penekanan tombol mulai.
     run_detection = st.button("Mulai")
 
-    # Creating a loop to capture each frame of the video in the name of Img
+    # Digunakan untuk menjalankan pendeteksian melalui kamera device jika tombol mulai ditekan.
     if run_detection:
+        # Digunakan untuk memulai pendeteksian melalui kamera.
         cap = cv2.VideoCapture(0)
+        # Jika kamera tidak bisa dibuka maka akan memberikan peringatan berupa text .
         if not cap.isOpened():
             st.write("Unable to access camera")
             st.stop()
-
+        
+        # Menjalankan proses pendeteksian kamera serta melakukan proses pendeteksian objek / benda.
         while True:
             _, img = cap.read()
 
-            # Converting to grey scale
+            # Digunakan untuk mengubah hasil deteksi menjadi grayscale.
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            # Allowing multiple face detection
+            # Digunakan untuk mendeteksi muka kucing.
             faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+            # Digunakan untuk mendeteksi muka manusia.
             human = human_cascade.detectMultiScale(gray, 1.1, 6)
 
+            # Digunakan untuk mendeteksi setiap bagian citra dan melakukan perulangan untuk memberikan tanda persegi dan label "Cat" pada daerah disekita muka kucing.
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 250), 2)
                 cv2.putText(img, "Cat", (x, y - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
-
+            
+            # Digunakan untuk mendeteksi setiap bagian citra dan melakukan perulangan untuk memberikan tanda persegi dan label "Human" pada daerah disekita muka manusia.
             for (x, y, w, h) in human:
                 cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 250), 2)
                 cv2.putText(img, "Human", (x, y - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
-            # Displaying the image
+            # Menampilkan hasil deteksi dari proses sebelumnya lalu ditampilkan secara format BGR (Berwarna).
             cv2.imshow('Detected Face Image',  img)
 
-            # Checking if the window is closed
+            # Digunakan untuk memeriksa apakah window yang menampilkan hasil kamera / memeriksa apakah kamera dijalankan atau tidak.
             if cv2.getWindowProperty('Detected Face Image', cv2.WND_PROP_VISIBLE) < 1:
                 break
 
-            # Waiting for escape key for image to close adding the break statement to end the face detection screen
+            # Digunakan untuk memberhentikan kamera dan window yang menampilkan hasil kamera.
             k = cv2.waitKey(30) & 0xff
             if k == 27:
                 break
 
-        # Real-time releasing the captured frames
+        # Menghapus semua hasil frame yang dihasilkan dari kamera dari device.
         cap.release()
 
-        # Closing all windows
+        # Digunakan untuk menutup semua windows / halaman yang berkaitan dengan aplikasi.
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
--- pages/Image_Detection.py
+++ pages/Image_Detection.py
@@ -2,8 +2,9 @@ import cv2
 import streamlit as st
 import numpy as np
 
-# Importing HARR CASCADE XML file
+# Digunakan untuk melakukan impor data haarcascade dari file xml untuk data muka kucing.
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+# Digunakan untuk melakukan impor data haarcascade dari file xml untuk data muka manusia.
 human_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
@@ -23,34 +24,42 @@ text2 = "Anda dapat menggunakan aplikasi ini untuk mengamati keberadaan wajah ma
 
 
 def main():
+    # Digunakan untuk memberikan title pada halaman.
     st.title("Human and Cat Face Image Detector")
+    # Digunakan untuk menampilkan data penjelesan dari variable text1 diatas.
     st.write(f"<p class='text-justify'>{text1}</p>", unsafe_allow_html=True)
+    # Digunakan untuk menampilkan data penjelesan dari variable text2 diatas.
     st.write(f"<p class='text-justify'>{text2}</p>", unsafe_allow_html=True)
+    # Digunakan untuk melakukan proses upload citra yang ingin dideteksi oleh program.
     uploaded_file = st.file_uploader("Pilih gambar yang akan dideteksi")
+    # Memeriksa keberadaan citra (apakah ada atau tidak) dari hasil proses upload sebelumnya.
     if uploaded_file is not None:
         st.write("You have uploaded a file.")
         file_bytes = np.asarray(
             bytearray(uploaded_file.read()), dtype=np.uint8)
         img = cv2.imdecode(file_bytes, 1)
 
-        # Converting to grey scale
+        # Mengubah citra menjadi grayscale.
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Allowing multiple face detection
+        # Digunakan untuk mendeteksi muka kucing.
         faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+        # Digunakan untuk mendeteksi muka manusia.
         human = human_cascade.detectMultiScale(gray, 1.1, 6)
 
+        # Digunakan untuk mendeteksi setiap bagian citra dan melakukan perulangan untuk memberikan tanda persegi dan label "Cat" pada daerah disekita muka kucing.
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 250), 2)
             cv2.putText(img, "Cat", (x, y - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
+        # Digunakan untuk mendeteksi setiap bagian citra dan melakukan perulangan untuk memberikan tanda persegi dan label "Human" pada daerah disekita muka manusia.
         for (x, y, w, h) in human:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 250), 2)
             cv2.putText(img, "Human", (x, y - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
-        # Displaying the uploaded image
+        # Menampilkan hasil deteksi dari proses sebelumnya lalu ditampilkan secara format BGR (Berwarna).
         st.image(img, channels="BGR", caption="Uploaded Image")
 
 
